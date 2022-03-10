78f02122345a69315fb66e9a7e31e73800626db6
VincentArellano <vincentvarellano08@gmail.com>
2022-03-10 10:02:14 +0000

    login_signup added

--- code/server/data/initdb.js
+++ code/server/data/initdb.js
@@ -19,7 +19,7 @@ let query = [
 "DROP TABLE IF EXISTS article_tag",
 "CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL, thumbnailURL TEXT, mediaType TEXT, mediaURL TEXT, leadStory INTEGER)",
 "CREATE TABLE moreArticle (id INTEGER PRIMARY KEY AUTOINCREMENT, moreContent TEXT NOT NULL)",
-"CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, password TEXT NOT NULL, name TEXT NOT NULL, accessLevel INTEGER NOT NULL)",
+"create table user(id   integer primary key,username varchar,password varchar);",
 "CREATE TABLE tag (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)",
 "CREATE TABLE article_tag (id INTEGER PRIMARY KEY AUTOINCREMENT, idArticle INTEGER REFERENCES article(id), idTag INTEGER REFERENCES tag(id))",
 "INSERT INTO article (title, content, thumbnailURL, mediaType, mediaURL) values ('Welcome to ecoal22', '<h1>Hello from ecoal</h1><p>Nice to see you in <strong>Lens</strong>. Enjoy !</p>', 'ecoal.jpg', 'image', 'ecoal.jpg')",
@@ -27,7 +27,7 @@ let query = [
 "INSERT INTO tag (name) values ('reactJS')",
 "INSERT INTO article_tag (idArticle, idTag) values (1,1)",
 "INSERT INTO article_tag (idArticle, idTag) values (1,2)",
-"INSERT INTO users (name, email, password, accessLevel) values ('Admin', '@admin@admin.com', '890*()qweQWE', 2)",
+"INSERT INTO users (name, email, password) values ('Admin', '@admin@admin.com', '890*()qweQWE')",
 ]
 
 
