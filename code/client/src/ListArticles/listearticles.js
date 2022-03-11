c7573f0f271528bd68855fdf8366ad07cf567fef
erwan <bmaxpro@gmail.com>
2022-03-11 18:25:05 +0100

    file update

--- code/client/src/ListArticles/listearticles.js
+++ code/client/src/ListArticles/listearticles.js
@@ -32,7 +32,7 @@ function updateData(e) {
 
 	console.log(myData)
 	if (myNbPlayer.toUpperCase() != "ALL") {
-		myData = myData.filter( (article) =>  article.nbplayer == myNbPlayer)
+		myData = myData.filter( (article) =>  article.nbplayer == myNbPlayer.toUpperCase())
 	} 
 		
 	if (myType.toUpperCase() != "ALL") {
@@ -61,7 +61,7 @@ return (
         <option>Ireland</option>
         <option>Portugal</option>
         <option>Spain</option>
-        <option>United State</option>
+        <option>United Kingdom</option>
         <option>Italy</option>
       </select>
 
@@ -75,6 +75,7 @@ return (
         <option>7</option>
         <option>8</option>
         <option>9</option>
+        <option>+10</option>
       </select>
 
 
@@ -82,6 +83,7 @@ return (
       	<option>All</option>
         <option>CardGame</option>
         <option>BoardGame</option>
+        <option>OutdoorGame</option>
       </select>
 
 
--- code/client/src/article/Article.js
+++ code/client/src/article/Article.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import axios from 'axios';
+import {useCookies} from 'react-cookie';
+import './Article.css';
 
 export default function Article() {
  
@@ -8,6 +10,9 @@ export default function Article() {
   const [data, setData] = useState( null );
 
   let params  = useParams();
+
+  const [cookies] = useCookies(['login']);
+  const isConnected = cookies.login&&cookies.login.username;
 
   async function getArticle(id) {
     const data = (await axios.get('http://localhost:8000/article/'+ params.id)).data;
@@ -18,27 +23,42 @@ export default function Article() {
     getArticle(params.id)
    }, []);
 
-  
-
    function displayMedia(type, url) {
-     return <img src={"http://localhost:8000/media/" +url} />
+     return <img className="img" src={"http://localhost:8000/media/"+url}/>
    }
 
+   function displayMedia2(type, url) {
+     return <img src={"http://localhost:8000/media/"+url}/>
+   }
+
+
    if (data == null)
-   	return <p>loading </p>;
-   
-   console.log(data);
+   	return <p>loading</p>;
+  
+ 
+   console.log(isConnected);
 
    let art = data[0];
 
-  return (<div>
-  	<article key={art.id}>
-  	 <h1> Il y a plus d'information </h1>
+   if (isConnected) {
+
+    return (<div>
+    {displayMedia(art.mediaType, art.moreContent)}
+    </div>);
+
+
+   }else{
+
+    return (<div>
+    <article key={art.id}>
+     <h1> Il y a plus d'information </h1>
       <h1 className="Article_title">{art.title}</h1>
-      <section dangerouslySetInnerHTML={{__html: art.content}}></section>
-      {displayMedia(art.mediaType,art.mediaURL)}
+      <section dangerouslySetInnerHTML={{__html: art.content}}>
+      </section>
+      {displayMedia2(art.mediaType, art.mediaURL)}
    </article>
-  	</div>
-  	);
+    </div>);
+
+  }
 
 }
